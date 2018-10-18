7404786dfa1f9a72ce4ac68ec9f4beffe5824b55
aleksspeaker <aleksspeaker@gmail.com>
2018-10-18 23:17:09 +0300

    Fixed one test and removed redundand

--- fe/src/reducers/reducer.test.ts
+++ fe/src/reducers/reducer.test.ts
@@ -1,67 +1,51 @@
-// import { TODOS } from '../actions/actions';
-// import { IState, reducer } from "./reducer";
+import { TODOS } from '../actions/actions';
+import { IState, reducer } from "./reducer";
 
-// let state: IState;
-// beforeEach(() => {
-//   state = {
-//     nextId: 2,
-//     todos: [
-//       {
-//         description: 'Learn Redux',
-//         done: false,
-//         id: 0,
-//       },
-//       {
-//         description: 'Learn React',
-//         done: false,
-//         id: 1,
-//       }
-//     ]
-//   }
-// })
+let state: IState;
+beforeEach(() => {
+  state = {
+    todos: [
+      {
+        _id: '0',
+        description: 'Learn Redux',
+        done: false,
+      },
+      {
+        _id: '1',
+        description: 'Learn React',
+        done: false,
+      }
+    ]
+  }
+})
 
-// describe('Reducer', () => {
-//   it('shoud return default state if action type is undefined', () => {
-//     expect(reducer(state, { type: undefined })).toEqual(state)
-//   })
-//   it('shoud update the nextId when ADD_TODO is dispatched', () => {
-//     const prevState = {
-//       nextId: 4,
-//       todos: [{
-//         description: 'Learn Redux',
-//         done: false,
-//         id: 3,
-//       },]
-//     }
-//     const action = {
-//       payload: '',
-//       type: TODOS.ADD_TODO,
-//     }
-//     expect(reducer(prevState, action)!.nextId).toBe(5)
-//   })
-//   it('shoud return state with new object added if action is ADD_TODO', () => {
-//     const nextState = {
-//       nextId: 3,
-//       todos: [
-//         {
-//           description: 'Learn Redux',
-//           done: false,
-//           id: 0,
-//         },
-//         {
-//           description: 'Learn React',
-//           done: false,
-//           id: 1,
-//         },
-//         {
-//           description: 'Learn TS',
-//           done: false,
-//           id: 2,
-//         },
-//       ]
-//     }
-//     expect(reducer(state, { type: TODOS.ADD_TODO, payload: "Learn TS" })).toEqual(nextState)
-//   })
+describe('Reducer', () => {
+  it('shoud return default state if action type is undefined', () => {
+    expect(reducer(state, { type: undefined })).toEqual(state)
+  })
+  it('shoud return state with new object added if action is ADD_TODO', () => {
+    const nextState: IState = {
+      todos: [
+        {
+          _id: '0',
+          description: 'Learn Redux',
+          done: false,
+        },
+        {
+          _id: '1',
+          description: 'Learn React',
+          done: false,
+        },
+        {
+          _id: '2',
+          description: 'Learn TS',
+          done: false,
+        },
+      ]
+    }
+    const payload = { _id: '2', description: "Learn TS", done: false };
+    expect(reducer(state, { type: TODOS.ADD_TODO_SUCCESS, payload})).toEqual(nextState)
+  })
 //   it('shoud return state with specified todo id, done: true if action is TOGGLE_TODO', () => {
 //     const nextState = {
 //       nextId: 2,
